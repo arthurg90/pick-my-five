e345dcb43096bf950ebda96d53ee6e5482a9854a
Arthur G <agraham@WINDELL-9DU9KRP.localdomain>
2018-01-06 17:09:16 +0000

    Logo added, TODO cleanup and README

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 //A reusable header component taking props from the App parent component
 const Header = ({ title, subtitle }) => (
     <div className='page-header'>
-      <img className='logo' src={require('../img/FootyLogoWhite.png')} alt={'flying football logo'}/>
+      <img className='logo' src={require('../img/playerLogo.png')} alt={'flying football logo'}/>
       <h1> {title} {subtitle ? <small> {subtitle} </small> : null} </h1>
     </div>
 );
